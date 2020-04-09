3a83ae61032824f24eaf71a8e83af2e160d39563
JY <jason0824@foxmail.com>
2020-04-09 08:52:50 +0800

    添加项目文件。

---
+++ QuikSPHelper/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace QuikSPHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sltTable = new System.Windows.Forms.ComboBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIDAL = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDAL = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMethod = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtReturn = new System.Windows.Forms.TextBox();
+            this.label666 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cbsAction = new System.Windows.Forms.CheckedListBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sltTable
+            // 
+            this.sltTable.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sltTable.FormattingEnabled = true;
+            this.sltTable.Location = new System.Drawing.Point(23, 13);
+            this.sltTable.Name = "sltTable";
+            this.sltTable.Size = new System.Drawing.Size(159, 23);
+            this.sltTable.TabIndex = 0;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(56, 149);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(100, 25);
+            this.txtAuthor.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-4, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Author";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(321, 13);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(467, 25);
+            this.txtDescription.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "IDAL";
+            // 
+            // txtIDAL
+            // 
+            this.txtIDAL.Location = new System.Drawing.Point(56, 54);
+            this.txtIDAL.Name = "txtIDAL";
+            this.txtIDAL.Size = new System.Drawing.Size(100, 25);
+            this.txtIDAL.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "DAL";
+            // 
+            // txtDAL
+            // 
+            this.txtDAL.Location = new System.Drawing.Point(213, 54);
+            this.txtDAL.Name = "txtDAL";
+            this.txtDAL.Size = new System.Drawing.Size(100, 25);
+            this.txtDAL.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(328, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Method";
+            // 
+            // txtMethod
+            // 
+            this.txtMethod.Location = new System.Drawing.Point(389, 54);
+            this.txtMethod.Name = "txtMethod";
+            this.txtMethod.Size = new System.Drawing.Size(100, 25);
+            this.txtMethod.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(518, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Return";
+            // 
+            // txtReturn
+            // 
+            this.txtReturn.Location = new System.Drawing.Point(579, 54);
+            this.txtReturn.Name = "txtReturn";
+            this.txtReturn.Size = new System.Drawing.Size(100, 25);
+            this.txtReturn.TabIndex = 11;
+            // 
+            // label666
+            // 
+            this.label666.AutoSize = true;
+            this.label666.Location = new System.Drawing.Point(12, 110);
+            this.label666.Name = "label666";
+            this.label666.Size = new System.Drawing.Size(39, 15);
+            this.label666.TabIndex = 14;
+            this.label666.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(56, 100);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 25);
+            this.txtName.TabIndex = 13;
+            // 
+            // cbsAction
+            // 
+            this.cbsAction.CheckOnClick = true;
+            this.cbsAction.FormattingEnabled = true;
+            this.cbsAction.Items.AddRange(new object[] {
+            "SELECT",
+            "ADD",
+            "UPDATE"});
+            this.cbsAction.Location = new System.Drawing.Point(179, 100);
+            this.cbsAction.Name = "cbsAction";
+            this.cbsAction.Size = new System.Drawing.Size(120, 84);
+            this.cbsAction.TabIndex = 15;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(-1, 192);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(800, 530);
+            this.txtContent.TabIndex = 17;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(345, 128);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 18;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(499, 128);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 19;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 719);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.cbsAction);
+            this.Controls.Add(this.label666);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtReturn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMethod);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDAL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIDAL);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.sltTable);
+            this.Name = "Form1";
+            this.Text = "快速生成SP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox sltTable;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIDAL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDAL;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMethod;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtReturn;
+        private System.Windows.Forms.Label label666;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.CheckedListBox cbsAction;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ QuikSPHelper/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuikSPHelper
+{
+    public partial class Form1 : Form
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitTable();
+            for (int i = 0; i < cbsAction.Items.Count; i++)
+            {
+                cbsAction.SetItemChecked(i, true);
+            }
+        }
+
+        void InitTable()
+        {
+            sltTable.Items.Clear();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                //建立连接
+                conn.Open();
+
+                //创建SQL命令
+                SqlCommand queryCmd = new SqlCommand("SELECT [Name] FROM SysObjects WHERE XType='U' ORDER BY [Name]", conn);
+
+
+                //执行SQL命令
+                SqlDataReader reader = queryCmd.ExecuteReader();
+                //处理SQL命令结果
+                while (reader.Read())
+                {
+                    sltTable.Items.Add(reader[0]);
+                }
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (sltTable.SelectedItem == null)
+            {
+                MessageBox.Show("选表！");
+                return;
+            }
+            txtContent.Text = string.Empty;
+            string tName = sltTable.SelectedItem.ToString();
+            string spName = txtName.Text;
+            string sql = "SELECT * FROM V_GetTabDefine WHERE tName='" + tName + "'";
+            List<ColumnItem> columns = new List<ColumnItem>();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                //建立连接
+                conn.Open();
+
+                //创建SQL命令
+                SqlCommand queryCmd = new SqlCommand(sql, conn);
+
+
+                //执行SQL命令
+                SqlDataReader reader = queryCmd.ExecuteReader();
+                //处理SQL命令结果
+                while (reader.Read())
+                {
+                    columns.Add(new ColumnItem()
+                    {
+                        _name = reader["_name"].ToString(),
+                        _description = reader["_description"].ToString(),
+                        _pk = reader["_pk"].ToString().Equals("PK"),
+                        _type = reader["_type"].ToString(),
+                        _length = Convert.ToInt32(reader["_length"]),
+                        _places = Convert.ToInt32(reader["_places"])
+                    });
+                }
+            }
+
+            AppendText("-- =============================================");
+            AppendText("-- Author:" + txtAuthor.Text);
+            AppendText("-- Create date: " + DateTime.Now.ToString("yyyy.MM.dd"));
+            AppendText("-- Description:" + txtDescription.Text);
+            if (!string.IsNullOrEmpty(txtIDAL.Text))
+            {
+                AppendText("-- IDAL " + txtIDAL.Text);
+            }
+            if (!string.IsNullOrEmpty(txtDAL.Text))
+            {
+                AppendText("-- DAL " + txtDAL.Text);
+            }
+            if (!string.IsNullOrEmpty(txtMethod.Text))
+            {
+                AppendText("-- Method " + txtMethod.Text);
+            }
+            if (!string.IsNullOrEmpty(txtReturn.Text))
+            {
+                AppendText("-- Return " + txtReturn.Text);
+            }
+            AppendText("/*");
+            AppendText("EXEC " + spName);
+            string EXECParams = string.Empty;
+            foreach (var itm in columns)
+            {
+                if (itm._type.Contains("char"))
+                {
+                    EXECParams += ("," + c1(itm._name) + "=N''");
+                }
+                else if (itm._type.Contains("date"))
+                {
+                    EXECParams += ("," + c1(itm._name) + "='" + DateTime.Now.ToString("yyyy-MM-dd") + "'");
+                }
+                else
+                {
+                    EXECParams += ("," + c1(itm._name) + "=0");
+                }
+            }
+            AppendText(EXECParams.Substring(1));
+            AppendText("*/");
+            AppendText("-- =============================================");
+            AppendText("CREATE PROCEDURE [dbo].["+ spName + "]");
+            AppendText(ConvertParas(columns[0]), 1);
+            for (int i = 1; i < columns.Count; i++)
+            {
+                AppendText("," + ConvertParas(columns[i]),1);
+            }
+            AppendText("AS");
+            AppendText("BEGIN");
+            AppendText("SET NOCOUNT ON;", 1);
+            string actStr = string.Empty;
+            string tmpStr = string.Empty;
+            for (int i = 0; i < cbsAction.CheckedItems.Count; i++)
+            {
+                switch (cbsAction.CheckedItems[i].ToString())
+                {
+                    case "SELECT":
+                        AppendText("SELECT ", 2);
+                        AppendText(columns[0]._name + " " + c(columns[0]._name), 3);
+                        for (int x = 1; x < columns.Count; x++)
+                        {
+                            AppendText("," + columns[x]._name + " " + c(columns[x]._name), 3);
+                        }
+                        AppendText(" FROM " + tName, 2);
+                        AppendText(" WHERE 1=1 ", 2);
+                        AppendText("");
+                        break;
+                    case "ADD":
+                        AppendText("INSERT INTO " + tName, 2);
+                        tmpStr = "(" + columns[0]._name;
+                        for (int x = 1; x < columns.Count; x++)
+                        {
+                            tmpStr += "," + columns[x]._name;
+                        }
+                        tmpStr += ")";
+                        AppendText(tmpStr, 2);
+                        AppendText(" VALUES",2);
+                        tmpStr = "(" + c1(columns[0]._name);
+                        for (int x = 1; x < columns.Count; x++)
+                        {
+                            tmpStr += "," + c1(columns[x]._name);
+                        }
+                        tmpStr += ")";
+                        AppendText(tmpStr, 2);
+                        AppendText(" SELECT 0", 2);
+                        AppendText("");
+                        break;
+                    case "UPDATE":
+                        AppendText("UPDATE " + tName, 2);
+                        AppendText(" SET " + columns[0]._name + "=" + c1(columns[0]._name), 3);
+                        for (int x = 1; x < columns.Count; x++)
+                        {
+                            AppendText("," + columns[x]._name + "=" + c1(columns[x]._name), 3);
+                        }
+                        AppendText(" WHERE 1=1", 2);
+                        AppendText(" SELECT 0", 2);
+                        AppendText("");
+                        break;
+                }
+            }
+            AppendText("END");
+        }
+
+        private string ConvertParas(ColumnItem item)
+        {
+            string str = c1(item._name) + " as " + item._type;
+            switch (item._type)
+            {
+                case "int":
+                case "tinyint":
+                case "datetime":
+                case "bigint":
+                case "bit":
+                    break;
+                default:
+                    if (item._places > 0)
+                    {
+                        str += "(" + item._length + "," + item._places + ")";
+                    }
+                    else
+                    {
+                        str += "(" + item._length + ")";
+                    }
+                    break;
+            }
+            str += "    " + "-" + item._description;
+            return str;
+        }
+
+        /// <summary>
+        /// 去前缀
+        /// </summary>
+        /// <param name="p">表字段</param>
+        /// <returns></returns>
+        private string c(string p)
+        {
+            return p.Substring(p.IndexOf('_'));
+        }
+
+        /// <summary>
+        /// 去前缀，加@前缀
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        private string c1(string p)
+        {
+            return "@" + c(p);
+        }
+
+        /// <summary>
+        /// 追加内容到文本框
+        /// </summary>
+        /// <param name="str">内容</param>
+        /// <param name="indentCount">缩进次数</param>
+        private void AppendText(string str, int indentCount = 0)
+        {
+            for (int i = 0; i < indentCount; i++)
+            {
+                txtContent.AppendText("    ");
+            }
+            txtContent.AppendText(str);
+            txtContent.AppendText(System.Environment.NewLine);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (txtContent.Text.Length > 0)
+            {
+                Clipboard.SetDataObject(txtContent.Text);
+                MessageBox.Show("已将内容复制到剪贴板");
+            }
+        }
+    }
+
+
+
+    /// <summary>
+    /// 表字段
+    /// </summary>
+    public class ColumnItem
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string _name { get; set; }
+        /// <summary>
+        /// 说明
+        /// </summary>
+        public string _description { get; set; }
+        /// <summary>
+        /// 是否为主键
+        /// </summary>
+        public bool _pk { get; set; }
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public string _type { get; set; }
+        /// <summary>
+        /// 长度
+        /// </summary>
+        public int _length { get; set; }
+        /// <summary>
+        /// 小数位
+        /// </summary>
+        public int _places { get; set; }
+    }
+}
